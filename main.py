eb320347f9b773a7a4989ee076366e6033b6edfa
Sharan-vj <sharanvj678@gmail.com>
2024-05-23 12:06:16 +0530

    feat: base model added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tumorClassifier import logger
 from tumorClassifier.pipe.stage_01_data_ingestion import DataIngestionPipeline
+from tumorClassifier.pipe.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion'
 if __name__ == '__main__':
@@ -8,6 +9,18 @@ if __name__ == '__main__':
         obj = DataIngestionPipeline()
         obj.main()
         logger.info(f'>>>>> Stage {STAGE_NAME} Completed <<<<<')
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = 'Prepare Base Model'
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
     except Exception as e:
         logger.exception(e)
         raise e
