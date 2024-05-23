5c77edbf003bfa02fc9e6b1d7a0f37c8a75afdbc
Sharan-vj <sharanvj678@gmail.com>
2024-05-23 12:12:10 +0530

    feat: model training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tumorClassifier import logger
 from tumorClassifier.pipe.stage_01_data_ingestion import DataIngestionPipeline
 from tumorClassifier.pipe.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from tumorClassifier.pipe.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion'
 if __name__ == '__main__':
@@ -19,6 +20,18 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "Model Training"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
     except Exception as e:
